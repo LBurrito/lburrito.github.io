2b2ab60ea976abafc65adbb50a9bba7c78746007
Luca <lucabommar@gmail.com>
2018-07-07 16:16:41 -0700

    Add files via upload

---
+++ deckjs.js
@@ -0,0 +1,19 @@
+
+var dataSet = {}
+
+$.getJSON("dataset.json", function(data) {
+    console.log("found data!")
+    dataSet = data;
+})
+new deck.DeckGL({
+    container: 'container',
+    mapStyle:'https://free.tilehosting.com/styles/positron/style.json?key=U0iNgiZKlYdwvgs9UPm1',      
+    longitude: -122.45,
+    latitude: 37.8,
+    zoom: 12,
+    layers: [
+        new deck.ScatterplotLayer(
+            dataSet
+        )
+    ]
+});
