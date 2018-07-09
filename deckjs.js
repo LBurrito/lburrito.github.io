6f01b760154ebdd80115ab2cbf0e556e552039f6
Luca <lucabommar@gmail.com>
2018-07-09 00:19:21 -0700

    Add files via upload

--- deckjs.js
+++ deckjs.js
@@ -1,10 +1,4 @@
-
-
-/*$.getJSON("dataset.json", function(data) {
-    console.log("found data!");
-    flightData = data;
-    console.log(flightData);
-})*/
+import TripsLayer from './trips-layer';
 const colors = 
 [
     [255,237,0],
@@ -16,12 +10,13 @@ const colors =
 
 new deck.DeckGL({
     container: 'container',
-    mapStyle:'https://free.tilehosting.com/styles/positron/style.json?key=U0iNgiZKlYdwvgs9UPm1',      
+    mapboxApiAccessToken: 'pk.eyJ1IjoibHVjYWJvbW1hciIsImEiOiJjamphYzZxcDkzZzc0M3ZvNm45d3Y4NXd0In0.NW5KWw9ZJg6a0iQm5D7omw',
+    mapStyle:'mapbox://styles/mapbox/dark-v9',      
     longitude: -122.45,
     latitude: 37.8,
     zoom: 12,
     layers: [
-        /*new deck.ScatterplotLayer({
+        new deck.ScatterplotLayer({
             id: 'scatter-plot',
             data: 'https://uc6157d73b62c52d065ca2c02e42.dl.dropboxusercontent.com/cd/0/inline/AK4tck0OItPWxl6oD89Vw_t_8Q2f7Kw5bJcUgcn2sGbuIi5CblX1SOZEuZPgNpT1lKTxjM9XKX9oSMxdEtNkbMrhaQKQhgPC50-dWNlym_6TBJ_nIGdL9tXgVCt5OgEzvPYmrP28KhhHLXYXNe0gDk6nzJsafS9sRDDpe4-kX4CxpykbQ3a6uVf11ZWo_PKSBd0/file',
             //radiusScale: 1,
@@ -39,7 +34,7 @@ new deck.DeckGL({
                 return [d.position[0], d.position[1], d.altitude]
             },
             opacity: 0.05
-        }),*/
+        }),
         /*new deck.PointCloudLayer({
                 id: 'point-cloud-layer',
                 data: 'https://raw.githubusercontent.com/LBurrito/lburrito.github.io/master/multiFlight.json',
@@ -61,7 +56,7 @@ new deck.DeckGL({
                 onHover: ({object}) => setTooltip(object.position.join(', '))
 
           })*/
-          new deck.LineLayer({
+          /*new deck.LineLayer({
             id: 'flight-paths',
             data: 'https://ucda45ff6a692192b02ddbca1ef6.dl.dropboxusercontent.com/cd/0/inline/AK401YbW2JxzEHNVUH7C5MXYtMxw__n3A3nbI4J1hcOpJJYQXtAObrIKjUuebD80u7f6NcBlXmu4mdSnSjNZObY0RTZSEql57B49zpOVKLXKyTnz2FVwKCxCw-rATDdgXoltT496eqW1yXavGQY9YK-prrHEz5XXqzXJgzgXjek9y5WnvC9dPMp_KbCl6MpirIk/file',
             fp64: false,
@@ -79,7 +74,18 @@ new deck.DeckGL({
                 )
             ),
             opacity: 0.05,
-            stroke: 1
+            stroke: 3
+          })*/
+          new TripsLayer({
+            id: 'trips',
+            data: 'https://uc0d896c086a841cef537af659e4.dl.dropboxusercontent.com/cd/0/inline/AK5_ZZYzIAyu6ObcBcfpeq2Wh59kOfMj-RK1cyLDruZGM8XtPIkloZe3MWOBY6ooXUGskjEGR9fh3Zh0DaoUq839mgA3LJLPOMEj5IeBxD7YQ4cqcJG7XMUO4SoI3kfKpZwbFap1NNWvP4xneKPWUHdw1cxqDhVY0wNnod_R3eiBvqsxA0ZKnaP4STL39Lg6pfw/file',
+            getPath: d => {
+                return [d.position[0], d.position[1], d.time]
+            } ,
+            getColor: [253, 128, 93],
+            opacity: 0.3,
+            strokeWidth: 2,
+            trailLength: 120,
           })
     ]
 });
---
+++ testjs.js
@@ -0,0 +1,1 @@
+import {tripsLayer} from './trips-layer'
