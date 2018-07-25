45b980d2697856e025bfa419e2b8918b75c29017
Luca <lucabommar@gmail.com>
2018-07-25 14:29:43 -0700

    Add initial state

--- deckjs.js
+++ deckjs.js
@@ -15,11 +15,29 @@ var timeSlider = document.getElementById("timeSlider");
 var timeControl = document.getElementById("sliceControl");
 var timeOut = document.getElementById("time");
 var strokeSlider = document.getElementById("strokeSlider");
+
+
+deckgl = new deck.DeckGL({
+    container: 'container',
+    mapboxApiAccessToken: 'pk.eyJ1IjoibHVjYWJvbW1hciIsImEiOiJjamphYzZxcDkzZzc0M3ZvNm45d3Y4NXd0In0.NW5KWw9ZJg6a0iQm5D7omw',
+    mapStyle:'mapbox://styles/mapbox/dark-v9',      
+    longitude: -122.017863,
+    latitude: 37.430330,
+    zoom: 9,
+    layers: []
+});
+var     startDate = new Date(tmin*1000).toLocaleString('en-US');
+var     endDate = new Date(tmax*1000).toLocaleString('en-US');
+
+refresh();
+timeOut.innerHTML = "" + startDate + " <br> to <br> " + endDate; // Display the default slider value
+timeControl.value = "Show 24hr"
+
 timeSlider.oninput = function() {
     tmin = this.value;
     tmax = parseFloat(tmin) + 3600;
-    var startDate = new Date(tmin*1000).toLocaleString('en-US');
-    var endDate = new Date(tmax*1000).toLocaleString('en-US');
+    startDate = new Date(tmin*1000).toLocaleString('en-US');
+    endDate = new Date(tmax*1000).toLocaleString('en-US');
     refresh()
     timeOut.innerHTML = "" + startDate + " <br> to <br> " + endDate; // Display the default slider value
     timeControl.value = "Show 24hr"
@@ -56,7 +74,7 @@ function refresh(){
                 
                 if(d.time > tmin && d.time < tmax)
                 {
-                    return [color[0], color[1], color[2], 50];
+                    return [color[0], color[1], color[2], 60];
                 }
                 else{
                     return [0, 0, 0, 0];
@@ -81,34 +99,28 @@ function refresh(){
     deckgl.setProps({layers})
 }
 
-deckgl = new deck.DeckGL({
-    container: 'container',
-    mapboxApiAccessToken: 'pk.eyJ1IjoibHVjYWJvbW1hciIsImEiOiJjamphYzZxcDkzZzc0M3ZvNm45d3Y4NXd0In0.NW5KWw9ZJg6a0iQm5D7omw',
-    mapStyle:'mapbox://styles/mapbox/dark-v9',      
-    longitude: -122.017863,
-    latitude: 37.430330,
-    zoom: 9,
-    layers: [
-        /*new deck.ScatterplotLayer({
-            id: 'scatter-plot',
-            data: 'https://ucea5430f9b964c0a270b35fb77e.dl.dropboxusercontent.com/cd/0/inline/AK9UEmmAkB-VCn4A90jVlia2VbPAkB3yFhZL0AphZf7U68UWFo18bJ6CNVrY16stgdB-DOzB40GRx4zeDux-Z5XF5ARA-SqH3fi-880URZdumOoWBQZjZV8VOtBG9tpqheTVUY-orjHuRyeIwb8BxELh5xylFuc6swr2vt2UohhwQpGw-ORMjY0S12xmugYSPdw/file',
-            //radiusScale: 1,
-            radius: 1,
-            //getRadius: d => (d.altitude / 10000) * 300,//max altitude is 10000,
-            getColor: d => (d.altitude < 0 ? [0,0,0] :
-                                (d.altitude < 2500 ? colors[0] : 
-                                    (d.altitude < 5000 ? colors[2] : 
-                                        (d.altitude < 7500 ? colors[2] : colors[3])
+
+        var unusedLayers = [
+            new deck.ScatterplotLayer({
+                id: 'scatter-plot',
+                data: 'https://ucea5430f9b964c0a270b35fb77e.dl.dropboxusercontent.com/cd/0/inline/AK9UEmmAkB-VCn4A90jVlia2VbPAkB3yFhZL0AphZf7U68UWFo18bJ6CNVrY16stgdB-DOzB40GRx4zeDux-Z5XF5ARA-SqH3fi-880URZdumOoWBQZjZV8VOtBG9tpqheTVUY-orjHuRyeIwb8BxELh5xylFuc6swr2vt2UohhwQpGw-ORMjY0S12xmugYSPdw/file',
+                //radiusScale: 1,
+                radius: 1,
+                //getRadius: d => (d.altitude / 10000) * 300,//max altitude is 10000,
+                getColor: d => (d.altitude < 0 ? [0,0,0] :
+                                    (d.altitude < 2500 ? colors[0] : 
+                                        (d.altitude < 5000 ? colors[2] : 
+                                            (d.altitude < 7500 ? colors[2] : colors[3])
+                                        )
                                     )
-                                )
-                            ),         
-            radiusMinPixels: 0.5,
-            getPosition: d => {
-                return [d.position[0], d.position[1], d.altitude]
-            },
-            opacity: 0.05
-        }),*/
-        /*new deck.PointCloudLayer({
+                                ),         
+                radiusMinPixels: 0.5,
+                getPosition: d => {
+                    return [d.position[0], d.position[1], d.altitude]
+                },
+                opacity: 0.05
+            }),
+        new deck.PointCloudLayer({
                 id: 'point-cloud-layer',
                 data: 'multiFlight.json',
                 pickable: false,
@@ -128,18 +140,6 @@ deckgl = new deck.DeckGL({
                 lightSettings: {},
                 onHover: ({object}) => setTooltip(object.position.join(', '))
 
-          })*/
-    
-          /*new TripsLayer({
-            id: 'trips',
-            data: 'https://ucea5430f9b964c0a270b35fb77e.dl.dropboxusercontent.com/cd/0/inline/AK9UEmmAkB-VCn4A90jVlia2VbPAkB3yFhZL0AphZf7U68UWFo18bJ6CNVrY16stgdB-DOzB40GRx4zeDux-Z5XF5ARA-SqH3fi-880URZdumOoWBQZjZV8VOtBG9tpqheTVUY-orjHuRyeIwb8BxELh5xylFuc6swr2vt2UohhwQpGw-ORMjY0S12xmugYSPdw/file',
-            getPath: d => {
-                return [d.position[0], d.position[1], d.time]
-            } ,
-            getColor: [253, 128, 93],
-            opacity: 0.3,
-            strokeWidth: 2,
-            trailLength: 120,
-          })*/
-    ]
-});
+            })
+        ];
+
